dc4dcebb58276981bdac3e5dea271984a1d77338
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-19 15:43:59 -0500

    Added book cover information, added the book cover tsx file to add information on the cover of books along with adding a sample image to check if the picture of the cover for the book works. Next will be to add the cover of the books themselves.

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import BookCover from "@/components/BookCover";
 
-const BookOverview = ({title, author, genre, rating, totalCopies, availableCopies, description, color, cover}: Book) => {
+const BookOverview = ({
+                          title,
+                          author,
+                          genre,
+                          rating,
+                          totalCopies,
+                          availableCopies,
+                          description,
+                          color,
+                          cover
+                      }: Book) => {
     return <section className="book-overview">
+        {/*Add Book Information*/}
         <div className="flex flex-1 flex-col gap-5">
+            {/*Add Book Title*/}
             <h1>{title}</h1>
-
+            {/*Add Book Info*/}
             <div className="book-info">
                 <p>
                     By <span className="font-semibold text-light-200">{author}</span>
@@ -14,17 +28,45 @@ const BookOverview = ({title, author, genre, rating, totalCopies, availableCopie
                     Category:{" "}
                     <span className="font-semibold text-light-200">{genre}</span>
                 </p>
-
+                {/*Add Book Rating*/}
                 <div className="flex flex-row gap-1">
-                   <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+                    <Image src="/icons/star.svg" alt="star" width={22} height={22}/>
                     <p>{rating}</p>
                 </div>
             </div>
-
+            {/*Add Book Copies*/}
             <div className="book-copies">
                 <p>Total Books: <span>{totalCopies}</span></p>
                 <p>Available Books: <span>{availableCopies}</span></p>
             </div>
+            {/*Add Book Description*/}
+            <p className="book-description">
+                {description}
+            </p>
+            {/* Add Button*/}
+            <Button className="book-overview_btn">
+                <Image src="/icons/book.svg" alt="book" width={20} height={20}/>
+                <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
+            </Button>
+        </div>
+
+        <div className="relative flex flex-1 justify-center">
+            <div className="relative">
+                <BookCover
+                    variant="wide"
+                    className="z-10"
+                    coverColor={color}
+                    coverImage={cover}
+                />
+                <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+                    <BookCover
+                        variant="wide"
+                        coverColor={color}
+                        coverImage={cover}
+                    />
+                </div>
+            </div>
+
         </div>
     </section>
 }
