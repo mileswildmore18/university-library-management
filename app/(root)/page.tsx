759660741af08aa2bf1c025f5f39145a1cdd2028
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-19 14:19:36 -0500

    Added books into the types d ts file along with adding books in the index ts file. Also added the information of the overview of books. Next will be to add the books for people to see.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,15 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import {sampleBooks} from "@/constants";
 
 const Home = () => <>
-    <BookOverview/>
-    <BookList/>
+    <BookOverview {...sampleBooks[0]}/>
+    <BookList
+    title="Latest Books"
+    books={sampleBooks}
+    containerClassName="mt-28"
+    />
 </>;
 
 export default Home;
