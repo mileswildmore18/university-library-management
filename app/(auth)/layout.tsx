2764da0f5e189571c4772c8934c90e63e298d323
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-20 15:45:28 -0500

    Added autehnication folders along with the sign up and sign in page to start on the authentication for users.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+// Add the layout of the authentication
+import React, {ReactNode} from 'react'
+
+const Layout = ({children} : { children: ReactNode}) => {
+    return (
+        <div>{children}</div>
+    )
+}
+export default Layout
