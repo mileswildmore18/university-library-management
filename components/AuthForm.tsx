d1378c39bcfb93b1456834250dd7a72c52e83247
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-27 13:36:30 -0500

    Added the necessary schemas that will validate the credentials the suer puts into the form and check if their data they enter is correct on submit.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client"
 
-// Add the auth form generic type
-// interface Props<T extends FieldValues>
+import { zodResolver } from "@hookform/resolvers/zod";
+import {DefaultValues, FieldValues, SubmitHandler, useForm, UseFormReturn} from "react-hook-form"
+import { z, ZodType } from "zod"
+// Add the auth form generic type with default function of T
+interface Props<T extends FieldValues> {
+    schema: ZodType<T>;
+    defaultValues: T;
+    // Check if credentials from user fills are correct on sign in or sign up
+    onSubmit: (data: T) => Promise<{success: boolean, error?: string}>;
+    type: 'SIGN_IN | SIGN_UP';
+}
 
-const AuthForm = ({ type, schema, defaultValues, onSubmit}: Props) => {
+const AuthForm = <T extends FieldValues>({ type, schema, defaultValues, onSubmit}: Props<T>) => {
+    // 1. Define your form.
+    const form: UseFormReturn<T>=useForm({
+        resolver: zodResolver(schema),
+        defaultValues: defaultValues as DefaultValues<T>,
+    });
+
+    // 2. Define a submit handler.
+    const handleSubmit: SubmitHandler<T>= async (data) => {}
     return (
         <div>AuthForm -- {type}</div>
     )
