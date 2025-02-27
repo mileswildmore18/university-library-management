fe79ad3d3c4d0b99bc2bdba9b0924ac3305462d2
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-27 14:14:45 -0500

    Added the mapping of form to show the username the user adds to submit.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -54,23 +54,29 @@ const AuthForm = <T extends FieldValues>({
                 : 'Please complete all fields and upload a valid university ID to gain access to the library'}</p>
             {/* Add the form */}
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+                <form
+                    onSubmit={form.handleSubmit(handleSubmit)}
+                    className="space-y-6 w-full">
+
+                    {Object.keys(defaultValues).map((field) => (
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="shadcn" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    ))}
+
                     <Button type="submit">Submit</Button>
                 </form>
             </Form>
