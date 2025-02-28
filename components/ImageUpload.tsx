05a606448a67818a04cb82adf79f4b1107c57f3f
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-28 11:24:13 -0500

    Added the imagekit configuration from the public key in the env folder along with adding the reference to the ikUploader. Next will be to add the configuration to the IKUpload properties.

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import { IKImage, ImageKitProvider, IKUpload, } from "imagekitio-next";
+import {IKImage, ImageKitProvider, IKUpload,} from "imagekitio-next";
 import config from "@/lib/config";
+import {useRef, useState} from "react";
+
+// Get the imagekit configuration
+const {env: {imagekit: {publicKey, urlEndpoint}}} = config;
+
 
 // Upload image securely
 const authenticator = async () => {
     // Check if the authenticator is accepted or if there is an error
     try {
-        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`, );
+        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`,);
 
         // Check if the server gets a response
-        if(!response.ok) {
+        if (!response.ok) {
             const errorText = await response.text();
             // Throw an error message if response fails
             throw new Error(`Request failed with status ${response.status}: ${errorText}`);
@@ -18,17 +23,26 @@ const authenticator = async () => {
         // Get response that returns the data
         const data = await response.json();
 
-        const { signature, expire, token } = data;
+        const {signature, expire, token} = data;
 
-        return { token, expire, signature };
+        return {token, expire, signature};
     } catch (error: any) {
-     throw new Error(`Authentication request failed: ${error.message}`)
+        throw new Error(`Authentication request failed: ${error.message}`)
     }
 }
 
 const ImageUpload = () => {
-    return (
-        <div>ImageUpload</div>
-    )
+    // Add image reference
+    const ikUploadRef = useRef(null);
+    const [file, setFile] = useState<{ filePath: string} | null> (null);
+    // Add Image Upload
+    return <ImageKitProvider
+        publicKey={publicKey}
+        urlEndpoint={urlEndpoint}
+        authenticator={authenticator}>
+    <IKUpload
+
+    />
+    </ImageKitProvider>
 }
 export default ImageUpload
