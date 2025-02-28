93a8c84885525a6c92cf1d7cfc8d22dcff25dede
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-28 10:52:35 -0500

    Added the imagekit authentication to accept or decline the images user send to the server and sends a message. Also added some new folders for the authentication database that will hold the information users enter.

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,6 +1,30 @@
 'use client'
 
 import { IKImage, ImageKitProvider, IKUpload, } from "imagekitio-next";
+import config from "@/lib/config";
+
+// Upload image securely
+const authenticator = async () => {
+    // Check if the authenticator is accepted or if there is an error
+    try {
+        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`, );
+
+        // Check if the server gets a response
+        if(!response.ok) {
+            const errorText = await response.text();
+            // Throw an error message if response fails
+            throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+        }
+        // Get response that returns the data
+        const data = await response.json();
+
+        const { signature, expire, token } = data;
+
+        return { token, expire, signature };
+    } catch (error: any) {
+     throw new Error(`Authentication request failed: ${error.message}`)
+    }
+}
 
 const ImageUpload = () => {
     return (
