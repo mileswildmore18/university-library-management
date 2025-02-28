5ef5a8f3cf837be9e129077e283f4905f8d0fc02
Miles Wildmore <mileswildmore18@gmail.com>
2025-02-28 11:12:37 -0500

    Fixed an error on the config ts in the lib folder to remove the error from the route ts authentication folder.

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ const config = {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT,
         imagekit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-            urlEndpoint: process.env.NEXT_PUBLIC_IMAGE_URL_ENDPOINT,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGE_URL_ENDPOINT!,
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
         }
     }
