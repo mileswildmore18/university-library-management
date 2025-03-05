055a8b13814815fc1313c58f20ba89cb1d74bc7e
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-05 10:46:35 -0500

    fixed a issue in the drizzle.ts file by fixing the config.ts file for the API Keys.

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,12 @@
 const config = {
     env: {
-        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT,
+        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGE_URL_ENDPOINT!,
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
         },
-        databaseUrl: process.env.DATABASE_URL
+        databaseUrl: process.env.DATABASE_URL!
     }
 }
 
