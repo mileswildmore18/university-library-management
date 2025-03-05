0387ba49dfbe58c0f9fa2891235d5e3064350799
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-05 11:06:40 -0500

    Added the database run commands in the package json to make it easier to run the commands as well as testing to see if the drizzle database works when opened.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,13 +5,17 @@ import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import {sampleBooks} from "@/constants";
 
-const Home = () => <>
+const Home = () => {
+
+    return(
+ <>
     <BookOverview {...sampleBooks[0]}/>
     <BookList
     title="Latest Books"
     books={sampleBooks}
     containerClassName="mt-28"
     />
-</>;
-
+</>
+    );
+};
 export default Home;
