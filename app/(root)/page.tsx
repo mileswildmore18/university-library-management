3dc5d9529e20b5fc8d55eea13130118ff4e5deab
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-05 11:14:12 -0500

    Added the drizzle database along with adding it to the home page to show who is assigned to the database when logging in the information.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,10 +4,15 @@ import {Button} from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import {sampleBooks} from "@/constants";
+import {db} from "../../database/drizzle";
+import {users} from "../../database/schema";
 
-const Home = () => {
-
-    return(
+// Define the Home component with the database information
+const Home = async () => {
+    const result = await db.select().from(users);
+    // Print the database information
+    console.log(JSON.stringify(result, null, 2))
+    return (
  <>
     <BookOverview {...sampleBooks[0]}/>
     <BookList
