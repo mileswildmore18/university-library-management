f8d263cc546019c0b284ed3e5f210caef11c6c35
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-12 10:58:26 -0400

    Added the sign in credentials to see if the information the user typed in matches or not to redirect to the next page or go back to the sign in with error message.

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,6 +5,31 @@ import { eq } from "drizzle-orm";
 import {db} from "@/database/drizzle";
 import {users} from "@/database/schema";
 import { hash } from "bcryptjs";
+import {signIn} from "@/auth";
+
+// Add sign in with the user's credentials
+const signInWithCredentials = async (params: Pick<AuthCredentials, 'email' | 'password'>) => {
+    const { email, password } = params;
+    // try to find the user
+    try {
+    // check if the user exists or information is correct or not
+        const result = await signIn('credentials', {
+            email,
+            password,
+            redirect: false
+        });
+
+        if(result?.error) {
+            return { success: false, error: result.error };
+        }
+
+        return { success: true }
+    } catch (error) {
+        // display error message and return to sign in
+        console.log(error, "Sign in error");
+        return { success: false, error: "Sign in error" };
+    }
+}
 
 const signUp= async (params: AuthCredentials) => {
     const { fullName, email, universityId, password, universityCard } = params;
@@ -29,7 +54,12 @@ const signUp= async (params: AuthCredentials) => {
             universityId,
             password: hashedPassword,
             universityCard,
-        })
+        });
+
+        // sign in the user that is added in the database
+        await signInWithCredentials({ email, password });
+
+        return { success: true }
     // display error message if the user is not added
     } catch (error) {
         console.log(error, "Signup error");
