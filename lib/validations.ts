4c5cab60eec59894aa6e56e552874083010dd93e
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-12 11:20:02 -0400

    Fixed an error in the validation ts file on line 4 and 15 with the sign up and sign in schema in the correct position.

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 // Add validations for user signing up
-export const signInSchema = z.object({
+export const signUpSchema = z.object({
     // Add information that requires 3 characters to create a new username along with email and university and card
     fullName: z.string().min(3),
     email: z.string().email(),
@@ -12,7 +12,7 @@ export const signInSchema = z.object({
     password: z.string().min(8),
 })
 // Add validations for user signing in
-export const signUpSchema = z.object({
+export const signInSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8),
 })
